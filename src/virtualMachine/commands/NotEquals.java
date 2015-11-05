bb2c455d280c03e060e15f3b4b3f40f320510d8b
Make-It-Work <swhjenni@student.avans.nl>
2015-11-05 11:59:32 +0100

    minion1

--- src/virtualMachine/commands/NotEquals.java
+++ src/virtualMachine/commands/NotEquals.java
@@ -13,10 +13,23 @@ public class NotEquals extends AbstractCommand {
     @Override
     public void Execute(VirtualMachine vm, List<Variable> parameters) {
         // TODO Auto-generated method stub
+        Object returnWaarde;
         Variable left = vm.variables.get(parameters.get(0).getWaarde());
         Variable right = vm.variables.get(parameters.get(1).getWaarde());
-        boolean result = !left.getWaarde().equals(right.getWaarde());
-        vm.returnValue = new Variable(Identifier.BOOL, result);
+
+        if(isIntGeneric(left) && isIntGeneric(right)) {
+            int intLeft = toInt(left);
+            int intRight = toInt(right);
+            returnWaarde = intLeft != intRight;
+            vm.returnValue = new Variable(Identifier.BOOL, returnWaarde);
+        }
+        else {
+            try {
+                throw new Exception("BEEDOOBEEDOO - Can only Compare numbers");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
